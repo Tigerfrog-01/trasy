ebd5a1e2d44ac1d716986b5489eb68937f5da6f3
KŽ <kristopherzuravljov005@gmail.com>
2025-09-23 15:56:04 +0300

    Add files via upload

---
+++ vormid/lugemineScript.js
@@ -0,0 +1,12 @@
+function tervitus() {
+    let vastus = document.getElementById("vastus");
+    let nimi = document.getElementById("nimi");
+
+    vastus.innerHTML = "Tere tulemast ja vasta küsimustule " + nimi.value+"!"+
+        " <br>sa õpid juba "+aastat.value+"aastat "; vastus.style.color="red";
+
+
+    {
+
+    }
+}
